6e573a240ee12b1020718479d4a1b367ff5f0912
Ammar <ammarhafiz222@gmail.com>
2025-12-01 21:42:59 +0500

    First commit (Created Packages)

---
+++ src/main/java/com/pulse/ecommerce/PulseEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.pulse.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulseEcommerceApplication.class, args);
+	}
+
+}
