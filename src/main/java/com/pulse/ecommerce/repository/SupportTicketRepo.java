cd4624bea8f2b0cd5624ac5228708f6cc125df31
Ammar <ammarhafiz222@gmail.com>
2025-12-10 18:09:22 +0500

    Changes security Config

---
+++ src/main/java/com/pulse/ecommerce/repository/SupportTicketRepo.java
@@ -0,0 +1,8 @@
+package com.pulse.ecommerce.repository;
+
+import com.pulse.ecommerce.model.SupportTicket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupportTicketRepo extends JpaRepository<SupportTicket,Long> {
+
+}
